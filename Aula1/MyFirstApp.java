ccfb16bb27f443d4c8cce7db7d6a2f0710ee9dc6
annapss <annpss07@gmail.com>
2022-06-15 10:20:24 -0300

    Arquivos da aula 1

---
+++ Aula1/MyFirstApp.java
@@ -0,0 +1,13 @@
+
+public class MyFirstApp {
+	public static void main(String[] args) {
+		System.out.println("Meu primeiro programa em Java..."); // é o print
+		System.out.println("Hello world!");
+		System.out.println("Nos somos a Laura e a Anna Paula");
+		int numero = 5; // declarando variável
+		//rodando o código no terminal
+		System.out.println(numero);
+		System.out.println("A soma é: " + (Integer.parseInt(args[0]) + Integer.parseInt(args[1])));
+		System.out.println("Olá, " + args[0] + " e " + args[1] + " " + args[2] );
+	}
+}
